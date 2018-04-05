f99e68feca1d1691f6c51ff0fb6561253b03fa85
wesley samuel <padagantla.padagantla@gmail.com>
2018-04-05 11:16:46 -0500

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const CSSExtract = new ExtractTextPlugin('styles.css');
   return {
     entry: './src/app.js',
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'public', 'dist'),
     filename: 'bundle.js'
   },
   module: {
@@ -42,7 +42,8 @@ const CSSExtract = new ExtractTextPlugin('styles.css');
   devtool: isProduction ? 'source-map':'inline-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
+    historyApiFallback: true,
+    publicPath: '/dist/'
   }
 }
   
